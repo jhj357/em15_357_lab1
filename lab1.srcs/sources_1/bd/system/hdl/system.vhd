2be8a3a04fc92c993fe3f2a7c5be6feac18c69b4
jhj357 <2595367641@qq.com>
2015-06-08 20:10:25 +0800

    lab1

---
+++ lab1.srcs/sources_1/bd/system/hdl/system.vhd
@@ -0,0 +1,112 @@
+--Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2014.4 (win64) Build 1071353 Tue Nov 18 18:24:04 MST 2014
+--Date        : Sun Mar 29 20:52:32 2015
+--Host        : USER-20120726WN running 64-bit Service Pack 1  (build 7601)
+--Command     : generate_target system.bd
+--Design      : system
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity system is
+  port (
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC
+  );
+end system;
+
+architecture STRUCTURE of system is
+  component system_processing_system7_0_0 is
+  port (
+    MIO : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    DDR_CAS_n : inout STD_LOGIC;
+    DDR_CKE : inout STD_LOGIC;
+    DDR_Clk_n : inout STD_LOGIC;
+    DDR_Clk : inout STD_LOGIC;
+    DDR_CS_n : inout STD_LOGIC;
+    DDR_DRSTB : inout STD_LOGIC;
+    DDR_ODT : inout STD_LOGIC;
+    DDR_RAS_n : inout STD_LOGIC;
+    DDR_WEB : inout STD_LOGIC;
+    DDR_BankAddr : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_Addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_VRN : inout STD_LOGIC;
+    DDR_VRP : inout STD_LOGIC;
+    DDR_DM : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_DQ : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_DQS_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_DQS : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    PS_SRSTB : inout STD_LOGIC;
+    PS_CLK : inout STD_LOGIC;
+    PS_PORB : inout STD_LOGIC
+  );
+  end component system_processing_system7_0_0;
+  signal processing_system7_0_DDR_ADDR : STD_LOGIC_VECTOR ( 14 downto 0 );
+  signal processing_system7_0_DDR_BA : STD_LOGIC_VECTOR ( 2 downto 0 );
+  signal processing_system7_0_DDR_CAS_N : STD_LOGIC;
+  signal processing_system7_0_DDR_CKE : STD_LOGIC;
+  signal processing_system7_0_DDR_CK_N : STD_LOGIC;
+  signal processing_system7_0_DDR_CK_P : STD_LOGIC;
+  signal processing_system7_0_DDR_CS_N : STD_LOGIC;
+  signal processing_system7_0_DDR_DM : STD_LOGIC_VECTOR ( 3 downto 0 );
+  signal processing_system7_0_DDR_DQ : STD_LOGIC_VECTOR ( 31 downto 0 );
+  signal processing_system7_0_DDR_DQS_N : STD_LOGIC_VECTOR ( 3 downto 0 );
+  signal processing_system7_0_DDR_DQS_P : STD_LOGIC_VECTOR ( 3 downto 0 );
+  signal processing_system7_0_DDR_ODT : STD_LOGIC;
+  signal processing_system7_0_DDR_RAS_N : STD_LOGIC;
+  signal processing_system7_0_DDR_RESET_N : STD_LOGIC;
+  signal processing_system7_0_DDR_WE_N : STD_LOGIC;
+  signal processing_system7_0_FIXED_IO_DDR_VRN : STD_LOGIC;
+  signal processing_system7_0_FIXED_IO_DDR_VRP : STD_LOGIC;
+  signal processing_system7_0_FIXED_IO_MIO : STD_LOGIC_VECTOR ( 53 downto 0 );
+  signal processing_system7_0_FIXED_IO_PS_CLK : STD_LOGIC;
+  signal processing_system7_0_FIXED_IO_PS_PORB : STD_LOGIC;
+  signal processing_system7_0_FIXED_IO_PS_SRSTB : STD_LOGIC;
+begin
+processing_system7_0: component system_processing_system7_0_0
+    port map (
+      DDR_Addr(14 downto 0) => DDR_addr(14 downto 0),
+      DDR_BankAddr(2 downto 0) => DDR_ba(2 downto 0),
+      DDR_CAS_n => DDR_cas_n,
+      DDR_CKE => DDR_cke,
+      DDR_CS_n => DDR_cs_n,
+      DDR_Clk => DDR_ck_p,
+      DDR_Clk_n => DDR_ck_n,
+      DDR_DM(3 downto 0) => DDR_dm(3 downto 0),
+      DDR_DQ(31 downto 0) => DDR_dq(31 downto 0),
+      DDR_DQS(3 downto 0) => DDR_dqs_p(3 downto 0),
+      DDR_DQS_n(3 downto 0) => DDR_dqs_n(3 downto 0),
+      DDR_DRSTB => DDR_reset_n,
+      DDR_ODT => DDR_odt,
+      DDR_RAS_n => DDR_ras_n,
+      DDR_VRN => FIXED_IO_ddr_vrn,
+      DDR_VRP => FIXED_IO_ddr_vrp,
+      DDR_WEB => DDR_we_n,
+      MIO(53 downto 0) => FIXED_IO_mio(53 downto 0),
+      PS_CLK => FIXED_IO_ps_clk,
+      PS_PORB => FIXED_IO_ps_porb,
+      PS_SRSTB => FIXED_IO_ps_srstb
+    );
+end STRUCTURE;
